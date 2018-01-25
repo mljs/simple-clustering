7171b93d1e0b2e850e1c28cec56c7aa7f8204793
jetwell <9850727+jetwell@users.noreply.github.com>
2018-01-25 11:49:36 +0100

    Added threshold support to vector parameters.

--- test/index.js
+++ test/index.js
@@ -19,6 +19,7 @@ describe('Test', function () {
 
         values.should.be.eql([[[1,2], [1,2]], [[3,4], [5,6]]]);
     });
+
     it('Trivial matrix 2 x 4', function () {
         let matrix = [
             [1,1,0,0],
@@ -36,6 +37,7 @@ describe('Test', function () {
                 [[5,6]]
             ]);
     });
+
     it('Trivial matrix 4 x 2', function () {
         let matrix = [
             [1,0],
@@ -55,6 +57,7 @@ describe('Test', function () {
                 [[5],[6]]
             ]);
     });
+
     it('Trivial matrix 2 x 2', function () {
         let matrix = [
             [1,0],
@@ -86,4 +89,103 @@ describe('Test', function () {
         
         values.should.be.eql(matrix);
     });
+
+	it('Clusters a simple similarity matrix and outputs indexes', function () {
+		let options = {
+			threshold: 0,
+			out: 'indexes'
+		};
+
+		let matrix = [
+			[1, 1, 0, 0, 0, 0],
+			[1, 1, 0, 0, 0, 0],
+			[0, 0, 1, 1, 0, 0],
+			[0, 0, 1, 1, 0, 0],
+			[0, 0, 0, 0, 1, 1],
+			[0, 0, 0, 0, 1, 1],
+		];
+
+		let values = simpleClustering(matrix, options);
+
+		values.should.be.eql(
+			[
+				[0,1],
+				[2,3],
+				[4,5]
+			]
+		);
+	});
+
+	it('Clusters a more complex similarity matrix and outputs indexes', function () {
+		let options = {
+			threshold: 0,
+			out: 'indexes'
+		};
+
+		let matrix = [
+			[1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+			[1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+			[0.0, 0.0, 1.0, 0.7, 0.0, 0.0],
+			[0.0, 0.0, 0.7, 1.0, 0.0, 0.0],
+			[0.0, 0.0, 0.0, 0.0, 1.0, 0.3],
+			[0.0, 0.0, 0.0, 0.0, 0.3, 1.0],
+		];
+
+		let values = simpleClustering(matrix, options);
+
+		values.should.be.eql(
+			[
+				[0,1],
+				[2,3],
+				[4,5]
+			]
+		);
+	});
+
+	it('Clusters a similarity matrix with several permutations and threshold above 0', function () {
+		let options = {
+			threshold: 0.3,
+			out: 'indexes'
+		};
+
+		let matrix = [
+			[1.0, 0.2, 1.0, 0.1, 0.0, 0.2],
+			[0.2, 1.0, 0.2, 0.3, 0.7, 0.1],
+			[1.0, 0.2, 1.0, 0.0, 0.1, 0.0],
+			[0.1, 0.3, 0.0, 1.0, 0.0, 0.3],
+			[0.1, 0.7, 0.0, 0.1, 1.0, 0.0],
+			[0.2, 0.0, 0.0, 0.3, 0.0, 1.0],
+		];
+
+		let values = simpleClustering(matrix, options);
+
+		values.should.be.eql(
+			[
+				[0,2],
+				[1,4],
+				[3],
+				[5]
+			]
+		);
+	});
+
+	it('Clusters a vector representing a similarity matrix with a threshold above 0', function () {
+		let options = {
+			threshold: 0.3,
+			out: 'indexes'
+		};
+
+		let vector = [1.0, 0.2, 1.0, 0.1, 0.0, 0.2, 1.0, 0.2, 0.3, 0.7, 0.1, 1.0, 0.0, 0.1, 0.0, 1.0, 0.0, 0.3, 1.0, 0.0, 1.0];
+
+		let values = simpleClustering(vector, options);
+
+		values.should.be.eql(
+			[
+				[0,2],
+				[1,4],
+				[3],
+				[5]
+			]
+		);
+	});
 });
